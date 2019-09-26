7648476b377813a9d668538b5a5624963fb0fdad
Unknown <adilabbas6@yahoo.com>
2019-09-26 11:27:02 -0400

    added model and database for members

---
+++ ADSBackend/Data/ADSBackendContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADSBackend.Models
+{
+    public class ADSBackendContext : DbContext
+    {
+        public ADSBackendContext (DbContextOptions<ADSBackendContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ADSBackend.Models.Member> Member { get; set; }
+    }
+}
