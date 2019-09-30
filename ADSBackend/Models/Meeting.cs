e3b2986991a05654eb07cf113016d33b4032fff2
Unknown <adilabbas6@yahoo.com>
2019-09-30 09:41:42 -0400

    meeting class

---
+++ ADSBackend/Models/Meeting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ADSBackend.Models
+{
+    public class Meeting
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime Start { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime End { get; set; }
+        public string Password { get; set; }
+        public List<int> MemberIds { get; set; } 
+    }
+}
