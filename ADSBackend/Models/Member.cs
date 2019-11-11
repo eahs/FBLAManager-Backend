fe1891a3915ea13df8735d6b1062980d949fb44c
Unknown <adilabbas6@yahoo.com>
2019-11-11 10:31:34 -0500

    updated member class

--- ADSBackend/Models/Member.cs
+++ ADSBackend/Models/Member.cs
@@ -13,6 +13,24 @@ namespace ADSBackend.Models
 
         public string Username { get; set; }
 
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; } = "PA";
+
+        public string ZipCode { get; set; }
+
+        public int Grade { get; set; }
+
+        public string RecruitedBy { get; set; }
+
         public string Email { get; set; }
 
         public string Phone { get; set; }
