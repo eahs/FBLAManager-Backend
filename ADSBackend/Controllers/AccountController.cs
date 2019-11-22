fd18835c5ea0d89163db4d93b642fe7893215a9c
Unknown <adilabbas6@yahoo.com>
2019-11-21 21:13:16 -0500

    uncommented email sending in user registration

--- ADSBackend/Controllers/AccountController.cs
+++ ADSBackend/Controllers/AccountController.cs
@@ -225,9 +225,9 @@ namespace ADSBackend.Controllers
 
                     _logger.LogInformation("User created a new account with password.");
 
-                    /*var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
-                    await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);*/
+                    await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
 
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation("User created a new account with password.");
